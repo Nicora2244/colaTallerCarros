718463619b80459cfe23ff51b4386e60020d0403
Nicolas Ramirez Arango <nicolas2000.24a@hotmail.com>
2023-08-18 19:53:46 -0500

    commit 2 Nico
    Se agregaron datos iniciales en clientes.

--- src/datos/Clientes.java
+++ src/datos/Clientes.java
@@ -10,5 +10,63 @@ package datos;
  * @author nicolas.ramirez_ara
  */
 public class Clientes {
-    
+
+    private String nombreD;
+    private String modeloC;
+    private int tiempoEsperaC;
+
+    /**
+     * Get the value of tiempoEsperaC
+     *
+     * @return the value of tiempoEsperaC
+     */
+    public int getTiempoEsperaC() {
+        return tiempoEsperaC;
+    }
+
+    /**
+     * Set the value of tiempoEsperaC
+     *
+     * @param tiempoEsperaC new value of tiempoEsperaC
+     */
+    public void setTiempoEsperaC(int tiempoEsperaC) {
+        this.tiempoEsperaC = tiempoEsperaC;
+    }
+
+    /**
+     * Get the value of modeloC
+     *
+     * @return the value of modeloC
+     */
+    public String getModeloC() {
+        return modeloC;
+    }
+
+    /**
+     * Set the value of modeloC
+     *
+     * @param modeloC new value of modeloC
+     */
+    public void setModeloC(String modeloC) {
+        this.modeloC = modeloC;
+    }
+
+    /**
+     * Get the value of nombreD
+     *
+     * @return the value of nombreD
+     */
+    public String getNombreD() {
+        return nombreD;
+    }
+
+    /**
+     * Set the value of nombreD
+     *
+     * @param nombreD new value of nombreD
+     */
+    public void setNombreD(String nombreD) {
+        this.nombreD = nombreD;
+    }
+
 }
